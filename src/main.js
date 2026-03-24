dfcf49f4aab1a0fa7072d081682449e3dc2c6bbc
mariiaplakhtyna <plaxtinamaria@gmail.com>
2026-03-24 11:23:28 -0400

    fix imports

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
-import './style.css';
+import './css/style.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import { getImagesByQuery } from './pixabay-api';
+import { getImagesByQuery } from './js/pixabay-api.js';
 import {
   createGallery,
   clearGallery,
   showLoader,
   hideLoader,
   lightbox,
-} from './render-functions';
-
-console.log('JS працює');
+} from './js/render-functions.js';
 
 const form = document.querySelector('.form');
 
@@ -30,7 +28,8 @@ form.addEventListener('submit', async event => {
 
     if (data.hits.length === 0) {
       iziToast.show({
-        message: 'No images found',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
         position: 'topRight',
       });
       return;
@@ -40,7 +39,7 @@ form.addEventListener('submit', async event => {
     lightbox.refresh();
   } catch (error) {
     iziToast.show({
-      message: 'Error loading images',
+      message: 'Something went wrong. Please try again later.',
       position: 'topRight',
     });
   } finally {
